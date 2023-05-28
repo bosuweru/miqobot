8cb57a63d2bf41d3a011026e2f2932895baa726a
Allan <116328571+bosuweru@users.noreply.github.com>
2023-05-28 17:01:31 -0500

    Update bot.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/client/bot.js
+++ src/client/bot.js
@@ -1,11 +1,26 @@
 "use strict";
 
+const fs = require("node:fs");
+const path = require("node:path");
+
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once("ready", (client) => {
-  console.log(`${client.user.tag} logged in.`);
-});
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+
+  if (event.once) {
+    client.on(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 client.login(process.env.SECRET_TOKEN);
