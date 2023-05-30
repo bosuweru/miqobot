d250976e1e9a9fe28001d3f089273651d77fe3f1
Allan <116328571+bosuweru@users.noreply.github.com>
2023-05-30 12:59:38 -0500

    Add interaction-create.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

---
+++ src/client/events/interaction-create.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const { Events } = require("discord.js");
+const { logger } = require("../../utils/pino");
+
+module.exports = {
+  name: Events.InteractionCreate,
+  once: false,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+      logger.warn(`No command matching ${interaction.commandName} was found.`);
+
+      return;
+    }
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      logger.error(error);
+
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({
+          content: "There was an error while executing this command.",
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: "There was an error while executing this command.",
+          ephemeral: true,
+        });
+      }
+    }
+  },
+};
