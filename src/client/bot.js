e084813ca9def17d6d2007654d7edbfab058d50d
Allan <116328571+bosuweru@users.noreply.github.com>
2023-05-30 10:43:55 -0500

    Update bot.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/client/bot.js
+++ src/client/bot.js
@@ -4,6 +4,7 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { logger } = require("../utils/pino");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -21,9 +22,7 @@ for (const file of commandFiles) {
   if ("data" in command && "execute" in command) {
     client.commands.set(command.data.name, command);
   } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-    );
+    logger.warn(`The command at ${filePath} is missing required properties.`);
   }
 }
 
