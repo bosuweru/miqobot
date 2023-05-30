1e09a52c216c62d139f1470fe6c7376dec039aee
Allan <116328571+bosuweru@users.noreply.github.com>
2023-05-29 22:12:12 -0500

    Update index.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 "use strict";
 
-const { load } = require("./utilities/dotenv");
-const { ShardingManager } = require("discord.js");
+/** @see {@link https://github.com/motdotla/dotenv#readme} */
+const { config } = require("dotenv");
+const { expand } = require("dotenv-expand");
 
-load();
+const result = config({
+  path: "private/.env",
+  debug: false,
+  encoding: "utf8",
+  override: false,
+});
+
+if (result.error) throw result.error;
+expand(result);
+
+/** @see {@link https://discordjs.guide/sharding/#getting-started} */
+const { ShardingManager } = require("discord.js");
 
 const manager = new ShardingManager("src/client/bot.js", {
   token: process.env.SECRET_TOKEN,
