646a88f532f2aa122d974ad1ecc79392b30019d0
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-03 12:21:06 -0500

    Update index.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -7,12 +7,28 @@ load();
 const { ShardingManager } = require("discord.js");
 const { logger } = require("./utils/pino");
 
+process
+  .on("uncaughtException", (error) => {
+    logger.fatal(`${error}`);
+    process.exit(-1);
+  })
+  .on("unhandledRejection", (reason) => {
+    logger.fatal(`${reason}`);
+    process.exit(-1);
+  });
+
 const manager = new ShardingManager("src/client/bot.js", {
   token: process.env.SECRET_TOKEN,
+  execArgv: ["--trace-warnings"],
+  shardArgs: ["--ansi", "--color"],
 });
 
 manager.on("shardCreate", (shard) => {
   logger.info(`Shard ${shard.id} launched.`);
+
+  shard.on("ready", () => {
+    logger.info(`Shard ${shard.id} online.`);
+  });
 });
 
 manager.spawn();
