4a5f764f2ac69f6aecf1cabba675bed65194e33d
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-03 12:21:26 -0500

    Update ping.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/client/commands/utility/ping.js
+++ src/client/commands/utility/ping.js
@@ -5,8 +5,16 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with pong!"),
+    .setDescription("Measures RTT and websocket heartbeat."),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    const sent = await interaction.reply({
+      content: "Pinging...",
+      fetchReply: true,
+    });
+    interaction.editReply(
+      `:stopwatch: RTT: ${
+        sent.createdTimestamp - interaction.createdTimestamp
+      }ms\n:heart: Websocket Heartbeat: ${interaction.client.ws.ping}ms`
+    );
   },
 };
