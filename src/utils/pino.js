3797609ef029f06a69f2ebd11fd737bc0eb3de1b
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-03 12:21:46 -0500

    Update pino.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/utils/pino.js
+++ src/utils/pino.js
@@ -3,10 +3,16 @@
 /** @see {@link https://getpino.io/} */
 const pino = require("pino");
 
-const transport = pino.transport({
-  target: "pino/file",
-  options: { destination: "./log.json" },
-});
-const logger = pino(transport);
+let logger;
+
+if (process.env.NODE_ENV === "development") {
+  logger = pino();
+} else {
+  const transport = pino.transport({
+    target: "pino/file",
+    options: { destination: "./logs/miqobot.log" },
+  });
+  logger = pino(transport);
+}
 
 module.exports = { logger };
