ced3342d392c6dc15a7b39e4371761d5db236017
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-06 16:34:26 -0500

    Update ping.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -8,16 +8,16 @@ class Ping extends Command {
     super(context, { ...options });
   }
 
-  #build = (int, res) => {
+  #embed = (text) => {
+    return new EmbedBuilder().setDescription(text);
+  };
+
+  #print = (int, res) => {
     const e = ":ping_pong:";
     const ws = bold(`${Math.round(int.client.ws.ping)}ms`);
     const rtt = bold(`${res.createdTimestamp - int.createdTimestamp}ms`);
 
     return `${e} The round-trip time is ${rtt}, and the websocket heartbeat is ${ws}.`;
-  };
-
-  #embed = (text) => {
-    return new EmbedBuilder().setDescription(text);
   };
 
   registerApplicationCommands(registry) {
@@ -29,15 +29,20 @@ class Ping extends Command {
   }
 
   async chatInputRun(interaction) {
+    this.container.logger.debug(`${interaction.user.tag} used /ping!`);
+
     const result = await interaction.reply({
       embeds: [this.#embed("Pinging...")],
       ephemeral: false,
       fetchReply: true,
     });
 
+    const string = this.#print(interaction, result);
     await interaction.editReply({
-      embeds: [this.#embed(this.#build(interaction, result))],
+      embeds: [this.#embed(string)],
     });
+
+    this.container.logger.debug(string);
   }
 }
 
