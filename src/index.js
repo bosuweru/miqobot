5bb5aa11f082d55c2711727faf8f1ca26ea25daf
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-06 20:26:03 -0500

    Update index.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@
 
 require("@sapphire/plugin-logger/register");
 const { GatewayIntentBits } = require("discord.js");
-const { LogLevel, SapphireClient } = require("@sapphire/framework");
+const { LogLevel, container, SapphireClient } = require("@sapphire/framework");
 
 const client = new SapphireClient({
-  intents: GatewayIntentBits.Guilds,
+  intents: [GatewayIntentBits.Guilds],
   logger: {
     level:
       process.env.NODE_ENV === "development" ? LogLevel.Debug : LogLevel.Info,
   },
 });
 
-client.login(process.env.SECRET_TOKEN);
+client.login(process.env.SECRET_TOKEN).catch((error) => {
+  const result = error.message;
+  container.logger.error(`Miqobot[index] ${result}`);
+});
