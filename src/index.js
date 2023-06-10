8a8239b04d4c99db55880912c354bb9333eedf59
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-10 15:02:23 -0500

    Update index.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -4,15 +4,46 @@ require("@sapphire/plugin-logger/register");
 const { GatewayIntentBits } = require("discord.js");
 const { LogLevel, container, SapphireClient } = require("@sapphire/framework");
 
-const client = new SapphireClient({
-  intents: [GatewayIntentBits.Guilds],
-  logger: {
-    level:
-      process.env.NODE_ENV === "development" ? LogLevel.Debug : LogLevel.Info,
-  },
-});
+class Miqobot {
+  constructor() {
+    this.client = new SapphireClient({
+      intents: [GatewayIntentBits.Guilds],
+      logger: {
+        level:
+          process.env.NODE_ENV === "production"
+            ? /* istanbul ignore next */ LogLevel.Info
+            : LogLevel.Debug,
+      },
+    });
+  }
 
-client.login(process.env.SECRET_TOKEN).catch((error) => {
-  const result = error.message;
-  container.logger.error(`Miqobot[index] ${result}`);
-});
+  connect() {
+    this.client.login(process.env.SECRET_TOKEN).catch((error) => {
+      /* istanbul ignore if  */
+      if (process.env.SECRET_TOKEN) {
+        const result = error.message;
+        container.logger.error(`Client[miqobot] ${result}`);
+      }
+    });
+  }
+
+  disconnect() {
+    try {
+      this.client.destroy();
+    } catch (error) {
+      /* istanbul ignore if  */
+      if (process.env.SECRET_TOKEN) {
+        const result = error.message;
+        container.logger.error(`Client[miqobot] ${result}`);
+      }
+    }
+  }
+}
+
+module.exports = { Miqobot };
+
+/* istanbul ignore if  */
+if (process.env.NODE_ENV) {
+  const miqobot = new Miqobot();
+  miqobot.connect();
+}
