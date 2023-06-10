66a1b5b6de87ef85e1786b7bb532e8fa1fd92152
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-10 16:40:16 -0500

    Add ready.spec.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

---
+++ src/test/events/ready.spec.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const sinon = require("sinon");
+const expect = require("chai").expect;
+
+const { Ready } = require("../../listeners/ready");
+const { ListenerStore } = require("@sapphire/framework");
+
+describe("Ready Listener", function () {
+  let stub = null;
+  let event = null;
+
+  after(function () {
+    expect(stub).to.not.be.null;
+    expect(event).to.not.be.null;
+    stub = null;
+    event = null;
+  });
+
+  before(function () {
+    expect(stub).to.be.null;
+    expect(event).to.be.null;
+    stub = sinon.spy(function () {
+      return sinon.createStubInstance(ListenerStore);
+    });
+    event = new Ready(stub);
+  });
+
+  describe("run()", function () {
+    it("...simulates running the listener", function () {
+      event.run();
+    });
+  });
+});
