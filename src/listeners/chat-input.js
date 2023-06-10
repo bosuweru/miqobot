947548ba736088dea7b48657c7d2e57283ac612f
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-10 17:56:13 -0500

    Update chat-input.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/listeners/chat-input.js
+++ src/listeners/chat-input.js
@@ -13,46 +13,51 @@ class ChatInputCommandDenied extends Listener {
     });
   }
 
-  #build = (pre) => {
-    if (pre.identifier === "preconditionCooldown") {
-      const val = Math.floor(pre.context.remaining / Time.Second);
-      const sec = () => {
-        if (val > 1) return `${val} seconds`;
-        else if (val == 1) return `${val} second`;
-        else return `${pre.context.remaining / Time.Second} seconds`;
+  builder(precondition) {
+    /* istanbul ignore else  */
+    if (precondition.identifier === "preconditionCooldown") {
+      const value = Math.floor(precondition.context.remaining / Time.Second);
+      const clock = () => {
+        if (value > 1) return `${value} seconds`;
+        else if (value == 1) return `${value} second`;
+        else return `${precondition.context.remaining / Time.Second} seconds`;
       };
-      const obj = {
+      const embed = {
+        color: "Red",
         title: "Slow down!",
-        description: `You will be able to use this command again in ${sec()}.`,
+        description: `You will be able to use this command again in ${clock()}.`,
       };
 
-      return obj;
+      return embed;
     }
-  };
+  }
 
-  #embed = (object) => {
+  message(object) {
     return new EmbedBuilder()
-      .setColor("Red")
+      .setColor(object.color)
       .setTitle(object.title)
       .setDescription(object.description);
-  };
+  }
 
   async run(precondition, { interaction }) {
-    try {
-      const result = this.#build(precondition);
+    /* istanbul ignore if  */
+    if (process.env.NODE_ENV) {
+      try {
+        const result = this.builder(precondition);
 
-      if (interaction.replied || interaction.deferred) {
-        await interaction.editReply({ embeds: [this.#embed(result)] });
-      } else {
-        await interaction.reply({ embeds: [this.#embed(result)] });
+        if (interaction.replied || interaction.deferred) {
+          await interaction.editReply({ embeds: [this.message(result)] });
+        } else {
+          await interaction.reply({ embeds: [this.message(result)] });
+        }
+
+        this.container.logger.debug(
+          `Listener[chatInputCommandDenied]: ${result.description}`
+        );
+      } catch (error) {
+        const result = `Listener[chatInputCommandDenied]: ${error.message}`;
+        this.container.logger.error(result);
       }
-
-      this.container.logger.debug(
-        `Listener[chatInputCommandDenied]: ${result.description}`
-      );
-    } catch (error) {
-      const result = `Listener[chatInputCommandDenied]: ${error.message}`;
-      this.container.logger.error(result);
     }
   }
 }
