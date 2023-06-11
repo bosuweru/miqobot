1fff82f800b33a043abb6e3d12ee165f3e1c7461
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-11 15:26:33 -0500

    Update index.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 "use strict";
+
+const path = require("node:path");
+
+const ShardingManager = require("discord.js").ShardingManager;
+
+class Crystals {
+  constructor(Crystal) {
+    this.manager = new ShardingManager(Crystal, {
+      mode: "process",
+      token: process.env.SECRET_TOKEN,
+      respawn: true,
+      execArgv: ["--trace-warnings"],
+      shardArgs: ["--ansi", "--color"],
+      shardList: "auto",
+      totalShards: "auto",
+    });
+  }
+
+  spawn() {
+    /* istanbul ignore if  */
+    if (process.env.NODE_ENV !== "staging") this.manager.spawn();
+
+    return undefined;
+  }
+
+  setupListener() {
+    /* istanbul ignore if  */
+    if (process.env.NODE_ENV !== "staging") this.manager.on();
+
+    return undefined;
+  }
+}
+
+module.exports = { Crystals };
+
+/* istanbul ignore next  */
+if (process.env.NODE_ENV !== "staging") {
+  const Client = path.join(__dirname, "client/crystal.js");
+  const Shards = new Crystals(Client);
+
+  Shards.setupListener();
+  Shards.spawn();
+}
