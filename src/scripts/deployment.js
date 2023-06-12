3d6b3cc72b0ede557352149daec27989f4eecb07
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-12 12:01:30 -0500

    Update deployment.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/scripts/deployment.js
+++ src/scripts/deployment.js
@@ -1,50 +1,48 @@
 "use strict";
 
+const fs = require("node:fs");
+const path = require("node:path");
+
+const REST = require("discord.js").REST;
+const Routes = require("discord.js").Routes;
+
+class Deploy {
+  constructor(directory) {
+    this.commands = [];
+    this.commandsPath = directory;
+    this.commandFiles = fs
+      .readdirSync(this.commandsPath)
+      .filter((file) => file.endsWith(".js"));
+  }
+
+  load() {
+    for (const file of this.commandFiles) {
+      const commandPath = path.join(this.commandsPath, file);
+      const { Command } = require(commandPath);
+
+      const command = new Command();
+      this.commands.push(command.data.toJSON());
+    }
+  }
+
+  save() {
+    const rest = new REST().setToken(process.env.SECRET_TOKEN);
+
+    if (process.env.NODE_ENV === "development")
+      rest.put(Routes.applicationGuildCommands("", ""), {
+        body: this.commands,
+      });
+    else rest.put(Routes.applicationCommands(""), { body: this.commands });
+  }
+}
+
+module.exports = { Deploy };
+
+/* istanbul ignore next  */
 if (process.env.NODE_ENV !== "staging") {
-  const fs = require("node:fs");
-  const path = require("node:path");
+  const folder = path.join(__dirname, "../client/commands");
+  const script = new Deploy(folder);
 
-  const REST = require("discord.js").REST;
-  const Routes = require("discord.js").Routes;
-
-  const commands = [];
-  const commandsPath = path.join(__dirname, "../client/commands");
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-
-  for (const file of commandFiles) {
-    const commandPath = path.join(commandsPath, file);
-    const { Command } = require(commandPath);
-
-    const command = new Command();
-    commands.push(command.data.toJSON());
-  }
-
-  const rest = new REST().setToken(process.env.SECRET_TOKEN);
-
-  if (process.env.NODE_ENV === "development") {
-    const { miqobot } = require("../../private/configuration/miqobot.json");
-
-    const client = miqobot.client;
-    const server = miqobot.server;
-
-    rest
-      .put(Routes.applicationGuildCommands(client.id, server.id), {
-        body: commands,
-      })
-      .then((data) => {
-        const result = `Successfully reloaded ${data.length} application (/) commands.`;
-        console.log(result);
-      });
-  } else {
-    const client = { id: process.env.CLIENT_ID };
-
-    rest
-      .put(Routes.applicationCommands(client.id), { body: commands })
-      .then((data) => {
-        const result = `Successfully reloaded ${data.length} application (/) commands.`;
-        console.log(result);
-      });
-  }
+  script.load();
+  script.save();
 }
