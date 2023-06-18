662674117186b87b73bbd8faec8fe0bccb28bf2f
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-18 10:58:21 -0500

    Update winston.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/utilities/winston.js
+++ src/utilities/winston.js
@@ -2,17 +2,28 @@
 
 const winston = require("winston");
 
+const level = process.env.NODE_ENV === "development" ? "debug" : "info";
+const levels = winston.config.npm.levels;
+const silent = process.env.NODE_ENV === "staging" ? true : false;
+const transports = [];
+const exitOnError = false;
+
 const logger = winston.createLogger({
-  level: process.env.NODE_ENV === "development" ? "debug" : "info",
-  levels: winston.config.npm.levels,
-  silent: false,
-  transports: [],
-  exitOnError: false,
+  level: level,
+  levels: levels,
+  silent: silent,
+  transports: transports,
+  exitOnError: exitOnError,
 });
 
 logger.add(
   new winston.transports.Console({
-    format: winston.format.combine(),
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.printf(({ level, message }) => {
+        return `[${level}] ${message}`;
+      })
+    ),
   })
 );
 
