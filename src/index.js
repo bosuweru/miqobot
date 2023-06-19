46aa8559e09ce70be825c55a1c4b63b6ae9efacb
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-19 11:04:39 -0500

    Update index.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,48 +1,23 @@
 "use strict";
 
 const path = require("node:path");
+
 const ShardingManager = require("discord.js").ShardingManager;
 
 const { logger } = require("./utilities/winston");
 
-class Crystals {
-  constructor(Crystal) {
-    this.manager = new ShardingManager(Crystal, {
-      mode: "process",
-      token: process.env.SECRET_TOKEN,
-      respawn: true,
-      execArgv: ["--trace-warnings"],
-      shardArgs: ["--ansi", "--color"],
-      shardList: "auto",
-      totalShards: "auto",
-    });
-  }
+const manager = new ShardingManager(path.join(__dirname, "client/miqobot.js"), {
+  mode: "process",
+  token: process.env.SECRET_TOKEN,
+  respawn: process.env.NODE_ENV === "production" ? true : false,
+  execArgv: ["--trace-warnings"],
+  shardArgs: ["--ansi", "--color"],
+  shardList: "auto",
+  totalShards: "auto",
+});
 
-  spawn() {
-    /* istanbul ignore if  */
-    if (process.env.NODE_ENV !== "staging") this.manager.spawn();
+manager.on("shardCreate", (shard) => {
+  logger.info(`Shard[${shard.id}]: The shard has been created.`);
+});
 
-    return undefined;
-  }
-
-  setupListener() {
-    /* istanbul ignore if  */
-    if (process.env.NODE_ENV !== "staging")
-      this.manager.on("shardCreate", (shard) => {
-        logger.info(`Shard ${shard.id} has been created.`);
-      });
-
-    return undefined;
-  }
-}
-
-module.exports = { Crystals };
-
-/* istanbul ignore next  */
-if (process.env.NODE_ENV !== "staging") {
-  const Client = path.join(__dirname, "client/crystal.js");
-  const Shards = new Crystals(Client);
-
-  Shards.setupListener();
-  Shards.spawn();
-}
+if (process.env.NODE_ENV !== "staging") manager.spawn();
