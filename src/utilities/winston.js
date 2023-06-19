4bc9ebbe985a9b5f84237e7f439e86d00f0e9b26
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-19 12:39:32 -0500

    Update winston.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/utilities/winston.js
+++ src/utilities/winston.js
@@ -3,21 +3,32 @@
 const winston = require("winston");
 
 const logger = winston.createLogger({
-  level: process.env.NODE_ENV === "development" ? "debug" : "info",
+  level:
+    process.env.NODE_ENV === "development"
+      ? /* istanbul ignore next */ "debug"
+      : "info",
   levels: winston.config.npm.levels,
-  silent: process.env.NODE_ENV === "staging" ? true : false,
+  silent:
+    process.env.NODE_ENV === "staging"
+      ? true
+      : /* istanbul ignore next */ false,
   transports: [],
-  exitOnError: process.env.NODE_ENV === "development" ? true : false,
+  exitOnError:
+    process.env.NODE_ENV === "development"
+      ? /* istanbul ignore next */ true
+      : false,
 });
 
 const consoleFormat = winston.format.combine(
   winston.format.colorize(),
   winston.format.timestamp({ format: "YYYY-MM-DD hh:mm:ss" }),
-  winston.format.printf(({ level, message, timestamp }) => {
-    if (process.env.NODE_ENV === "development")
-      return `${timestamp} [${level}] ${message}`;
-    else return `[${level}] ${message}`;
-  })
+  winston.format.printf(
+    /* istanbul ignore next */ ({ level, message, timestamp }) => {
+      if (process.env.NODE_ENV === "development")
+        return `${timestamp} [${level}] ${message}`;
+      else return `[${level}] ${message}`;
+    }
+  )
 );
 
 const consoleTransport = new winston.transports.Console({
