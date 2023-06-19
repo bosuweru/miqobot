0ad0bc690959ff071b0e49ebadbe074db5ebdf68
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-19 11:04:20 -0500

    Update client-ready.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/client/events/client-ready.js
+++ src/client/events/client-ready.js
@@ -4,20 +4,10 @@ const Events = require("discord.js").Events;
 
 const { logger } = require("../../utilities/winston");
 
-class Event {
-  constructor() {
-    this.name = Events.ClientReady;
-    this.once = true;
-  }
-
-  async execute(client) {
-    /* istanbul ignore if  */
-    if (process.env.NODE_ENV !== "staging") {
-      logger.info(`${client.user.tag} has logged in.`);
-    } else {
-      return null;
-    }
-  }
-}
-
-module.exports = { Event };
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    logger.info(`Event[${this.name}]: ${client.user.tag} has been logged in.`);
+  },
+};
