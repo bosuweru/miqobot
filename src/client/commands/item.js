47e6dd68dac12b3aa4335e9d908046ee65c686be
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-20 02:04:20 -0500

    Update item.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/client/commands/item.js
+++ src/client/commands/item.js
@@ -1,21 +1,11 @@
 "use strict";
 
 const Time = require("@sapphire/duration").Time;
+const Collection = require("discord.js").Collection;
 const EmbedBuilder = require("discord.js").EmbedBuilder;
 const SlashCommandBuilder = require("discord.js").SlashCommandBuilder;
 
-const { fetch } = require("../../api/xiv-item");
 const { logger } = require("../../utilities/winston");
-
-function onItem(data) {
-  return new EmbedBuilder()
-    .setColor("Green")
-    .setTitle(data.title)
-    .setFooter({ text: "XIVAPI", iconURL: "https://xivapi.com/favicon.png" })
-    .setThumbnail(data.thumbnail)
-    .setTimestamp()
-    .setDescription(data.description);
-}
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -30,12 +20,25 @@ module.exports = {
   cooldown: 10000 / Time.Second,
   async execute(interaction) {
     try {
+      const id = interaction.options.getString("id");
+
+      if (!interaction.client.xivcache.has(this.data.name))
+        interaction.client.xivcache.set(this.data.name, new Collection());
+
+      const expiration = 3600000 * Time.Millisecond;
+      const collection = interaction.client.xivcache.get(this.data.name);
+
+      if (collection.has(id))
+        return await interaction.reply({
+          content: `${collection.get(id).title}`,
+        });
+
+      collection.set(interaction.options.getString("id"), { title: "Cached!" });
+      setTimeout(() => collection.delete(id), expiration);
+
       await interaction.deferReply();
 
-      const id = interaction.options.getString("id");
-      const data = await fetch(id);
-
-      return await interaction.editReply({ embeds: [onItem(data)] });
+      return await interaction.editReply({ content: "Fetched!" });
     } catch (error) {
       const exception = `${error.message}`;
       logger.error(`Command[${this.data.name}]: ${exception}`);
