cac002da33ae6845e73e1fb353a95cb9c1dc130b
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-22 16:08:21 -0500

    Update client.spec.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/tests/client/client.spec.js
+++ src/tests/client/client.spec.js
@@ -1,4 +1,20 @@
 "use strict";
 
-const { client } = require("../../client/client");
-const { expect } = require("chai");
+const expect = require("chai").expect;
+const client = require("../../client/client").client;
+
+describe("src/client/client.js", function () {
+  describe("Client 'ready'", function () {
+    it("returns true if the event had a listener", function () {
+      const boolean = client.emit("ready", {});
+      expect(boolean).to.be.true;
+    });
+  });
+
+  describe("Client 'interactionCreate'", function () {
+    it("returns true if the event had a listener", function () {
+      const boolean = client.emit("interactionCreate", {});
+      expect(boolean).to.be.true;
+    });
+  });
+});
