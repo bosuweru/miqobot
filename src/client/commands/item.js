84970910c8fb2373ba9f382d2ed6a148c5894c6d
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-21 20:47:30 -0500

    Update item.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/client/commands/item.js
+++ src/client/commands/item.js
@@ -5,16 +5,18 @@ const { request } = require("undici");
 const { Second, Millisecond } = require("@sapphire/duration").Time;
 const { Collection, EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 
-async function fetch(args, ...params) {
-  try {
-    const url = `https://xivapi.com/${args}/${params[0]}?private_key=${process.env.XIVAPI_PRIVATE_KEY}`;
-    const req = await request(url);
+function build(result) {
+  const title = result.Name;
+  const thumbnail = result.IconHD ? result.IconHD : result.Icon;
+  const description = result.Description;
 
-    return await req.body.json();
-  } catch (error) {
-    const exception = `${error.message}`;
-    logger.error(`${exception}`);
-  }
+  return new EmbedBuilder()
+    .setColor("Green")
+    .setTitle(title)
+    .setFooter({ text: "XIVAPI", iconURL: "https://xivapi.com/favicon.png" })
+    .setThumbnail("https://xivapi.com" + thumbnail || null)
+    .setTimestamp()
+    .setDescription(description || "No description provided.");
 }
 
 function error() {
@@ -24,23 +26,26 @@ function error() {
     .setDescription("Please use the autocomplete feature.");
 }
 
-function build(result) {
-  return new EmbedBuilder()
-    .setColor("Green")
-    .setTitle(result.Name)
-    .setFooter({ text: "XIVAPI", iconURL: "https://xivapi.com/favicon.png" })
-    .setTimestamp()
-    .setDescription(result.Description || "No description provided.");
+async function fetch(args, ...kwargs) {
+  try {
+    const url = `https://xivapi.com/${args}/${kwargs[0]}?private_key=${process.env.XIVAPI_PRIVATE_KEY}`;
+    const req = await request(url);
+
+    return await req.body.json();
+  } catch (error) {
+    const exception = `${error.message}`;
+    logger.error(`${exception}`);
+  }
 }
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("item")
     .setDescription("Returns information about an item.")
-    .addStringOption((option) =>
+    .addIntegerOption((option) =>
       option
-        .setName("name")
-        .setDescription("The name of an item to search.")
+        .setName("id")
+        .setDescription("Enter the name of an item to search.")
         .setRequired(true)
         .setAutocomplete(true)
     ),
@@ -50,30 +55,23 @@ module.exports = {
       if (!interaction.client.xivcache.has(this.data.name))
         interaction.client.xivcache.set(this.data.name, new Collection());
 
-      const data = [];
-      const name = interaction.options.getString("name");
-      const list = interaction.client.datasets.get("item");
-      list.forEach((value, key) => {
-        if (name.localeCompare(value, "en", { sensitivity: "accent" }) == 0)
-          data.push({ id: key, name: value });
-      });
+      const id = interaction.options.getInteger("id");
+      const dataset = interaction.client.datasets.get(this.data.name);
 
-      if (!data.length)
+      if (!dataset.has(id))
         return await interaction.reply({
           embeds: [error()],
           ephemeral: true,
           fetchReply: false,
         });
 
-      const delay = 3600000 * Millisecond;
-      const items = interaction.client.xivcache.get(this.data.name);
-      const query = data[0];
+      const cache = interaction.client.xivcache.get(this.data.name);
 
-      if (items.has(query.id)) {
-        const result = items.get(query.id);
+      if (cache.has(id)) {
+        const query = cache.get(id);
 
         return await interaction.reply({
-          embeds: [build(result)],
+          embeds: [build(query)],
           ephemeral: false,
           fetchReply: false,
         });
@@ -81,11 +79,13 @@ module.exports = {
 
       await interaction.deferReply();
 
-      const result = await fetch(this.data.name, query.id);
-      items.set(query.id, result);
-      setTimeout(() => items.delete(query.id), delay);
+      const delay = 1800000 * Millisecond;
+      const query = await fetch(this.data.name, id);
 
-      return await interaction.editReply({ embeds: [build(result)] });
+      cache.set(id, query);
+      setTimeout(() => cache.delete(id), delay);
+
+      return await interaction.editReply({ embeds: [build(query)] });
     } catch (error) {
       const exception = `${error.message}`;
       logger.error(`${exception}`);
@@ -94,13 +94,17 @@ module.exports = {
   async autocomplete(interaction) {
     try {
       const focused = interaction.options.getFocused();
-      const dataset = interaction.client.datasets.get("item");
-      const choices = Array.from(dataset.values());
+      const dataset = interaction.client.datasets.get(this.data.name);
+      const choices = Array.from(dataset, ([key, value]) => ({ key, value }));
 
-      const filtered = choices.filter((choice) => choice.startsWith(focused));
+      const filtered = choices.filter((choice) =>
+        choice.value.toLowerCase().startsWith(focused.toLowerCase())
+      );
 
       return await interaction.respond(
-        filtered.map((choice) => ({ name: choice, value: choice })).slice(0, 25)
+        filtered
+          .map((choice) => ({ name: choice.value, value: choice.key }))
+          .slice(0, 25)
       );
     } catch (error) {
       const exception = `${error.message}`;
