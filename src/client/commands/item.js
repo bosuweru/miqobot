9ca611b28ab82641b60946abe20e88aadd0f6c3e
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-23 00:39:13 -0500

    Update item.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/client/commands/item.js
+++ src/client/commands/item.js
@@ -5,39 +5,6 @@ const { request } = require("undici");
 const { Second, Millisecond } = require("@sapphire/duration").Time;
 const { Collection, EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 
-function build(result) {
-  const title = result.Name;
-  const thumbnail = result.IconHD ? result.IconHD : result.Icon;
-  const description = result.Description;
-
-  return new EmbedBuilder()
-    .setColor("Green")
-    .setTitle(title)
-    .setFooter({ text: "XIVAPI", iconURL: "https://xivapi.com/favicon.png" })
-    .setThumbnail("https://xivapi.com" + thumbnail || null)
-    .setTimestamp()
-    .setDescription(description || "No description provided.");
-}
-
-function error() {
-  return new EmbedBuilder()
-    .setColor("Red")
-    .setTitle("Error")
-    .setDescription("Please use the autocomplete feature.");
-}
-
-async function fetch(args, ...kwargs) {
-  try {
-    const url = `https://xivapi.com/${args}/${kwargs[0]}?private_key=${process.env.XIVAPI_PRIVATE_KEY}`;
-    const req = await request(url);
-
-    return await req.body.json();
-  } catch (error) {
-    const exception = `${error.message}`;
-    logger.error(`${exception}`);
-  }
-}
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("item")
@@ -50,6 +17,36 @@ module.exports = {
         .setAutocomplete(true)
     ),
   cooldown: 5000 / Second,
+  build(item) {
+    const title = item.Name;
+    const thumbnail = item.IconHD ? item.IconHD : item.Icon;
+    const description = item.Description;
+
+    return new EmbedBuilder()
+      .setColor("Green")
+      .setTitle(title)
+      .setFooter({ text: "XIVAPI", iconURL: "https://xivapi.com/favicon.png" })
+      .setThumbnail("https://xivapi.com" + thumbnail || null)
+      .setTimestamp()
+      .setDescription(description || "No description provided.");
+  },
+  error() {
+    return new EmbedBuilder()
+      .setColor("Red")
+      .setTitle("Error")
+      .setDescription("Please use the autocomplete feature.");
+  },
+  async fetch(args, ...kwargs) {
+    try {
+      const url = `https://xivapi.com/${args}/${kwargs[0]}?private_key=${process.env.XIVAPI_PRIVATE_KEY}`;
+      const req = await request(url);
+
+      return await req.body.json();
+    } catch (error) {
+      const exception = `${error.message}`;
+      logger.error(`${exception}`);
+    }
+  },
   async execute(interaction) {
     try {
       if (!interaction.client.xivcache.has(this.data.name))
@@ -60,7 +57,7 @@ module.exports = {
 
       if (!dataset.has(id))
         return await interaction.reply({
-          embeds: [error()],
+          embeds: [this.error()],
           ephemeral: true,
           fetchReply: false,
         });
@@ -71,7 +68,7 @@ module.exports = {
         const query = cache.get(id);
 
         return await interaction.reply({
-          embeds: [build(query)],
+          embeds: [this.build(query)],
           ephemeral: false,
           fetchReply: false,
         });
@@ -80,12 +77,12 @@ module.exports = {
       await interaction.deferReply();
 
       const delay = 1800000 * Millisecond;
-      const query = await fetch(this.data.name, id);
+      const query = await this.fetch(this.data.name, id);
 
       cache.set(id, query);
       setTimeout(() => cache.delete(id), delay);
 
-      return await interaction.editReply({ embeds: [build(query)] });
+      return await interaction.editReply({ embeds: [this.build(query)] });
     } catch (error) {
       const exception = `${error.message}`;
       logger.error(`${exception}`);
