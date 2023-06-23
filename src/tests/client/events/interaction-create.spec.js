f0422bcd1c41e4523c68295f9345ddbc9c750af2
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-23 00:38:56 -0500

    Add interaction-create.spec.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

---
+++ src/tests/client/events/interaction-create.spec.js
@@ -0,0 +1,99 @@
+"use strict";
+
+const expect = require("chai").expect;
+const events = require("../../../client/events/interaction-create");
+
+const { Time } = require("@sapphire/duration");
+const { Collection } = require("discord.js");
+
+describe("src/client/events/interaction-create.js", function () {
+  let interaction;
+
+  afterEach(function () {
+    for (const key in interaction) delete interaction[key];
+  });
+
+  beforeEach(function () {
+    interaction = {
+      user: {
+        id: "1112420494411583488",
+      },
+      client: {
+        cooldown: new Collection(),
+        commands: {
+          get(string) {
+            return string === interaction.command.data.name
+              ? interaction.command
+              : false;
+          },
+        },
+      },
+      command: {
+        data: { name: "workflow" },
+        cooldown: 1000 / Time.Second,
+        execute(object) {
+          return object ? true : false;
+        },
+        autocomplete(object) {
+          return object ? true : false;
+        },
+      },
+      commandName: "workflow",
+      autocomplete: null,
+      chatInputCommand: null,
+      reply(object) {
+        return object ? true : false;
+      },
+      isAutocomplete() {
+        return this.autocomplete;
+      },
+      isChatInputCommand() {
+        return this.chatInputCommand;
+      },
+    };
+  });
+
+  describe("InteractionCreate 'execute'", function () {
+    it("returns undefined if command not found", async function () {
+      interaction.commandName = "NaN";
+      interaction.autocomplete = true;
+      interaction.chatInputCommand = false;
+
+      let result = await events.execute(interaction);
+      expect(result).to.be.undefined;
+
+      interaction.autocomplete = false;
+      interaction.chatInputCommand = true;
+
+      result = await events.execute(interaction);
+      expect(result).to.be.undefined;
+    });
+
+    it("returns true if interaction is autocomplete", async function () {
+      interaction.autocomplete = true;
+      interaction.chatInputCommand = false;
+
+      const result = await events.execute(interaction);
+      expect(result).to.be.true;
+    });
+
+    it("returns true if interaction is chat input command", async function () {
+      interaction.autocomplete = false;
+      interaction.chatInputCommand = true;
+
+      let result = await events.execute(interaction);
+      expect(result).to.be.true;
+
+      result = await events.execute(interaction);
+      expect(result).to.be.true;
+    });
+
+    it("returns undefined if interaction is not autocomplete or chat input command", async function () {
+      interaction.autocomplete = false;
+      interaction.chatInputCommand = false;
+
+      const result = await events.execute(interaction);
+      expect(result).to.be.undefined;
+    });
+  });
+});
