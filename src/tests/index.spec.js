387bd276cb07f8b5606aa3818003f169e293bc53
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-23 17:03:58 -0500

    Update index.spec.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -2,12 +2,12 @@
 
 require("../scripts/deployment");
 const expect = require("chai").expect;
-const manager = require("../index").manage;
+const manage = require("../index").manage;
 
 describe("src/index.js", function () {
   describe("ShardingManager 'shardCreate'", function () {
     it("returns true if event has listener attached", function () {
-      const boolean = manager.emit("shardCreate", { id: 0 });
+      const boolean = manage.emit("shardCreate", { id: 0 });
       expect(boolean).to.be.true;
     });
   });
