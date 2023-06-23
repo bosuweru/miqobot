d4e875011ee95fb574325727b12f3a6fd3a3683e
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-23 18:36:17 -0500

    Add interaction-create.mock.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

---
+++ src/tests/client/events/__mocks__/interaction-create.mock.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const { Time } = require("@sapphire/duration");
+const { Collection } = require("discord.js");
+
+// eslint-disable-next-line prettier/prettier
+const { miqobot } = require("../../../../assets/configuration/miqobot.configuration.json");
+
+// eslint-disable-next-line prettier/prettier
+const { workflow } = require("../../../../assets/configuration/workflow.configuration.json");
+const { interaction } = workflow;
+
+module.exports = {
+  set: {
+    commandName: "workflow",
+    autocomplete: null,
+    isChatInputCommand: null,
+  },
+  user: miqobot.client.development.id,
+  client: {
+    cooldown: new Collection(),
+    commands: new Collection(),
+  },
+  command: {
+    data: { name: "workflow" },
+    cooldown: 1000 / Time.Second,
+    execute(object) {
+      return object ? true : false;
+    },
+    autocomplete(object) {
+      return object ? true : false;
+    },
+  },
+  commandName: interaction.commandName,
+  reply(object) {
+    return object ? false : true;
+  },
+  isAutocomplete() {
+    return this.set.autocomplete;
+  },
+  isChatInputCommand() {
+    return this.set.chatInputCommand;
+  },
+};
