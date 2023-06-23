43ba37fa90490e5d61455d88d25823687bba5255
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-23 00:39:35 -0500

    Update ping.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/client/commands/ping.js
+++ src/client/commands/ping.js
@@ -4,41 +4,37 @@ const { logger } = require("../../utilities/winston");
 const { Second } = require("@sapphire/duration").Time;
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 
-/* istanbul ignore next */
-function onPing(message, interaction) {
-  const ws = Math.round(interaction.client.ws.ping);
-  const rtt = message.createdTimestamp - interaction.createdTimestamp;
-  const description = `The round-trip time is ${rtt}ms, and the websocket heartbeat is ${ws}ms.`;
-
-  return new EmbedBuilder()
-    .setColor("Green")
-    .setTitle("Ping")
-    .setDescription(description);
-}
-
-function onWait() {
-  return new EmbedBuilder()
-    .setColor("Default")
-    .setTitle("Ping")
-    .setDescription("Pinging...");
-}
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Measures RTT and websocket heartbeat."),
   cooldown: 3000 / Second,
+  onPing(message, interaction) {
+    const ws = Math.round(interaction.client.ws.ping);
+    const rtt = message.createdTimestamp - interaction.createdTimestamp;
+    const description = `The round-trip time is ${rtt}ms, and the websocket heartbeat is ${ws}ms.`;
+
+    return new EmbedBuilder()
+      .setColor("Green")
+      .setTitle("Ping")
+      .setDescription(description);
+  },
+  onWait() {
+    return new EmbedBuilder()
+      .setColor("Default")
+      .setTitle("Ping")
+      .setDescription("Pinging...");
+  },
   async execute(interaction) {
     try {
       const message = await interaction.reply({
-        embeds: [onWait()],
+        embeds: [this.onWait()],
         ephemeral: false,
         fetchReply: true,
       });
 
-      /* istanbul ignore next */
       return await interaction.editReply({
-        embeds: [onPing(message, interaction)],
+        embeds: [this.onPing(message, interaction)],
       });
     } catch (error) {
       const exception = `${error.message}`;
