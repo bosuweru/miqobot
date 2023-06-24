f0bfb65756fcacc0f185dff42e03b1eea92cdbe9
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-23 22:26:57 -0500

    Update ping.spec.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/tests/client/commands/ping.spec.js
+++ src/tests/client/commands/ping.spec.js
@@ -6,12 +6,12 @@ const params = require("./__mocks__/ping.mock");
 
 describe("src/client/commands/ping.js", function () {
   describe("Ping 'execute'", function () {
-    it("returns true if chat input command is executed successfully", async function () {
+    it("returns true if command is executed successfully", async function () {
       const result = await events.execute(params);
       expect(result).to.be.true;
     });
 
-    it("returns undefined if chat input command is executed unsuccessfully", async function () {
+    it("returns undefined if command is executed unsuccessfully", async function () {
       const result = await events.execute(null);
       expect(result).to.be.undefined;
     });
