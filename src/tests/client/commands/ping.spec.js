d50713305d86f4a9e3b1ee4a35e45cecbb3a0810
Allan <116328571+bosuweru@users.noreply.github.com>
2023-06-23 20:14:05 -0500

    Update ping.spec.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/tests/client/commands/ping.spec.js
+++ src/tests/client/commands/ping.spec.js
@@ -1,38 +1,18 @@
 "use strict";
 
 const expect = require("chai").expect;
-const command = require("../../../client/commands/ping");
+const events = require("../../../client/commands/ping");
+const params = require("./__mocks__/ping.mock");
 
 describe("src/client/commands/ping.js", function () {
-  let interaction;
-
-  afterEach(function () {
-    for (const key in interaction) delete interaction[key];
-  });
-
-  beforeEach(function () {
-    interaction = {
-      client: { ws: { ping: 0 } },
-      createdTimestamp: 0,
-      reply(object) {
-        return object ? { createdTimestamp: 0 } : null;
-      },
-      editReply(object) {
-        return object ? true : false;
-      },
-    };
-  });
-
   describe("Ping 'execute'", function () {
-    it("returns true if chat input command is executed", async function () {
-      const result = await command.execute(interaction);
+    it("returns true if chat input command is executed successfully", async function () {
+      const result = await events.execute(params);
       expect(result).to.be.true;
     });
 
-    it("returns undefined if chat input command is invalid", async function () {
-      interaction.client = undefined;
-
-      const result = await command.execute(interaction);
+    it("returns undefined if chat input command is executed unsuccessfully", async function () {
+      const result = await events.execute(null);
       expect(result).to.be.undefined;
     });
   });
